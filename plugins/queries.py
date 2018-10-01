1293743640f21bcda058fcfc5b44490b1dae86ab
FrenchMasterSword <frenchmastersword@gmail.com>
2018-10-01 19:57:50 +0200

    modified:   plugins/queries.py
    working pythondoc command

--- plugins/queries.py
+++ plugins/queries.py
@@ -4,7 +4,9 @@ import discord
 from discord.ext import commands
 from bs4 import BeautifulSoup
 from datetime import datetime
+from string import ascii_uppercase
 import random
+import re
 
 import stackexchange as se
 
@@ -45,6 +47,40 @@ class Search:
                 await ctx.send(embed=emb)
             else:
                 await ctx.send("No results")
+
+    @commands.command()
+    async def pythondoc(self, ctx, *, text: str):
+        """Filters python.org results based on your query"""
+
+        url = "https://docs.python.org/3/genindex-all.html"
+        alphabet = '_' + ascii_uppercase
+
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as client_session:
+                async with client_session.get(url) as response:
+                    if response.status != 200:
+                        return await ctx.send('An error occurred (status code: {response.status}). Retry later.')
+
+                    soup = BeautifulSoup(str(await response.text()), 'html.parser')
+
+                    def soup_match(tag):
+                        return all(string in tag.text for string in text.strip().split()) and tag.name == 'li'
+
+                    elements = soup.find_all(soup_match, limit=10)
+                    print(elements)
+                    links = [tag.select_one("li > a") for tag in elements]
+                    links = [link for link in links if link is not None]
+
+                    if not links:
+                        return await ctx.send("No results")
+
+                    content = [f"[{a.string}](https://docs.python.org/3/{a.get('href')})" for a in links]
+
+                    emb = discord.Embed(title="Python 3 docs")
+                    emb.add_field(name=f'Results for `{text}` :', value='\n'.join(content), inline=False)
+
+                    await ctx.send(embed=emb)
+
 
 def setup(bot):
     bot.add_cog(Search(bot))
