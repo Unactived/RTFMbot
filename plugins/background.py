57bbdd04c946365277eb02d536fe11c4c86c06d2
FrenchMasterSword <frenchmastersword@gmail.com>
2021-08-13 15:34:33 +0200

    Fix Background lists updating

--- plugins/background.py
+++ plugins/background.py
@@ -10,17 +10,17 @@ class Background(commands.Cog):
         self.languages_url = 'https://tio.run/languages.json'
         self.update_languages.start()
 
-        if bot.user.id == bot.config['ID']:
-            self.lists_settings = {
+        if bot.user.id == bot.config['ID']: # actual RTFM
+            self.lists_settings = (
                 # url: headers
-                f'https://discordbots.org/api/bots/{bot.config["ID"]}/stats':     {'Authorization' : bot.config['DB_TOKEN']},
-                f'https://botsfordiscord.com/api/bot/{bot.config["ID"]}':         {'Authorization' : bot.config['BFD_TOKEN']},
-                f'https://discord.bots.gg/api/v1/bots/{bot.config["ID"]}/stats':  {'Authorization' : bot.config['DBGG_TOKEN'], 'Content-Type': 'application/json'},
-                f'https://discordbotlist.com/api/bots/{bot.config["ID"]}/stats':  {'Authorization' : f"Bot {bot.config['DBL_TOKEN']}", 'Content-Type': 'application/json'}
-            }
+                (f'https://top.gg/api/bots/{bot.config["ID"]}/stats',                 {'Authorization' : bot.config['DB_TOKEN']}),
+                (f'https://discords.com/bots/api/bot/{bot.config["ID"]}',             {'Authorization' : bot.config['BFD_TOKEN']}),
+                (f'https://discord.bots.gg/api/v1/bots/{bot.config["ID"]}/stats',     {'Authorization' : bot.config['DBGG_TOKEN'], 'Content-Type': 'application/json'}),
+                (f'https://discordbotlist.com/api/v1/bots/{bot.config["ID"]}/stats',  {'Authorization' : bot.config['DBL_TOKEN'], 'Content-Type': 'application/json'})
+            )
             self.update_dbl_count.start()
 
-    @tasks.loop(hours=1)
+    @tasks.loop(hours=1.0)
     async def update_languages(self):
         async with aiohttp.ClientSession() as client_session:
             async with client_session.get(self.languages_url) as response:
@@ -32,15 +32,17 @@ class Background(commands.Cog):
                 if self.bot.languages != languages:
                     self.bot.languages = languages
 
-    @tasks.loop(hours=1)
+    @tasks.loop(minutes=30.0)
     async def update_dbl_count(self):
+        # grouped to avoid doing it ~4 times and maintain same stats among websites
         guildCount = len(self.guilds)
         usersCount = sum([guild.member_count for guild in self.guilds])
+        shardCount = self.shard_count
 
         lists_payloads = (p for p in [
+            {'server_count': guildCount, 'shard_count': shardCount},
             {'server_count': guildCount},
-            {'server_count': guildCount},
-            {'guildCount':   guildCount},
+            {'guildCount':   guildCount, "shardCount": shardCount},
             {'guilds':       guildCount, 'users': usersCount}
         ])
 
